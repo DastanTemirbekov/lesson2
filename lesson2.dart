fd6e57314df6ab99997705d6ee7ade7fe8977f38
Dastan Temirbekov <dastan.temirbekov@local.st.by>
2023-10-04 20:39:04 +0600

    first commit

---
+++ lesson2.dart
@@ -0,0 +1,17 @@
+void main(List<String> args) {
+  num a = 10;
+  print(a);
+  num a1 = 11.0;
+  print(a1);
+  num a2 = 15;
+
+  int b = 10;
+  int b1 = 1513241324;
+  int b2 = 12312;
+  print(b);
+  print(b1);
+  print(b2);
+
+  String c = 'asdasd';
+  String c1 = "asdasda";
+}
